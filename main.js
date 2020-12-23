665b39363a6c14d73725fa374cf646981aa206a8
Manuel254 <Immanuelkituku@gmail.com>
2020-12-23 15:36:52 +0300

    refactoring

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-let c = 0;
-let images = [];
-let time = 10000;
+// let c = 0;
+// let images = [];
+// let time = 10000;
 
-for (let i = 1; i < 11; i++) {
-    images.push(`images/slide${i}.jpeg`);
-}
+// for (let i = 1; i < 11; i++) {
+//     images.push(`images/slide${i}.jpeg`);
+// }
 
-function slider() {
-    document.slide.src = images[c];
+// function slider() {
+//     document.slide.src = images[c];
 
-    if (c < images.length - 1) {
-        c++;
-    }else {
-        c = 0;
-    }
-    setTimeout("slider()", time);
-}
+//     if (c < images.length - 1) {
+//         c++;
+//     }else {
+//         c = 0;
+//     }
+//     setTimeout("slider()", time);
+// }
 
-window.onload = slider;
+// window.onload = slider;
 
 
